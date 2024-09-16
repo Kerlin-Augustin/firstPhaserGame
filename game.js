2fcd11a21f8e5c4d49c75bf519d38e9fc8687113
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-09-16 13:37:50 -0400

    added coin asset

--- game.js
+++ game.js
@@ -1,13 +1,15 @@
 class mainScene {
   preload(){
     this.load.image('player', 'assets/player.png')
+    this.load.image('coin', 'assets/coin.png')
   }
   create(){
     this.player = this.physics.add.sprite(100, 100, 'player')
+    this.coin = this.physics.add.sprite(300, 300, 'coin')
   }
 
   update(){
-
+    this.score = 0;
   }
 }
 
