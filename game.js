2b196167a79baa488aabc40fad9d626f28ef8af3
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-09-16 13:41:34 -0400

    score calc

--- game.js
+++ game.js
@@ -4,12 +4,14 @@ class mainScene {
     this.load.image('coin', 'assets/coin.png')
   }
   create(){
-    this.player = this.physics.add.sprite(100, 100, 'player')
+    this.player = this.physics.add.sprite(100, 110, 'player')
     this.coin = this.physics.add.sprite(300, 300, 'coin')
   }
 
   update(){
     this.score = 0;
+    let style = { font: '20px Arial', fill: '#fff' };
+    this.scoreText = this.add.text(20, 20, 'score: ' + this.score, style)
   }
 }
 
